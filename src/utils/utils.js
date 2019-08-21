cf8b246cf6b1a67f3232809aef65bed42ef7f921
wangguoyun <wendywgyy@gmail.com>
2019-08-21 11:58:42 +0800

    日期

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -204,29 +204,33 @@ export function findRouterData (path, menu) {
   }
 }
 // 时间格式化
-// Date.prototype.Format = function (fmt) {
-//   var o = {
-//     'M+': this.getMonth() + 1, // 月份
-//     'd+': this.getDate(), // 日
-//     'h+': this.getHours(), // 小时
-//     'm+': this.getMinutes(), // 分
-//     's+': this.getSeconds(), // 秒
-//     'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
-//     S: this.getMilliseconds() // 毫秒
-//   }
-//   if (/(y+)/.test(fmt))
-//     fmt = fmt.replace(
-//       RegExp.$1,
-//       (this.getFullYear() + '').substr(4 - RegExp.$1.length)
-//     )
-//   for (var k in o)
-//     if (new RegExp('(' + k + ')').test(fmt))
-//       fmt = fmt.replace(
-//         RegExp.$1,
-//         RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
-//       )
-//   return fmt
-// }
+/* eslint no-extend-native: ["error", { "exceptions": ["Date"] }] */
+Date.prototype.Format = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1, // 月份
+    'd+': this.getDate(), // 日
+    'h+': this.getHours(), // 小时
+    'm+': this.getMinutes(), // 分
+    's+': this.getSeconds(), // 秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+    S: this.getMilliseconds() // 毫秒
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+    }
+  }
+  return fmt
+}
 
 // 多级数组转elementui数组格式
 export function exchangeData (data) {
@@ -352,23 +356,27 @@ export function getTodayStart () {
   return date
 }
 // 时间格式化
-// Date.prototype.Format = function (fmt) {
-//   var o = {
-//     'M+': this.getMonth() + 1,                 // 月份
-//     'd+': this.getDate(),                    // 日
-//     'h+': this.getHours(),                   // 小时
-//     'm+': this.getMinutes(),                 // 分
-//     's+': this.getSeconds(),                 // 秒
-//     'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
-//     'S': this.getMilliseconds()             // 毫秒
-//   };
-//   if (/(y+)/.test(fmt))
-//     fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-//   for (var k in o)
-//     if (new RegExp('(' + k + ')').test(fmt))
-//       fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
-//   return fmt
-// }
+/* eslint no-extend-native: ["error", { "exceptions": ["Date"] }] */
+Date.prototype.Format = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1, // 月份
+    'd+': this.getDate(), // 日
+    'h+': this.getHours(), // 小时
+    'm+': this.getMinutes(), // 分
+    's+': this.getSeconds(), // 秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+    'S': this.getMilliseconds() // 毫秒
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+    }
+  }
+  return fmt
+}
 // 今天结束时间
 export function getTodayEnd () {
   let date = new Date()
