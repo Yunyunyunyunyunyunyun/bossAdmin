4663b04abdb41c79cc054c0ce67b9bbca7aa7e54
wangguoyun <wendywgyy@gmail.com>
2019-08-22 18:39:55 +0800

    新建客户

--- src/views/customer/customerInfo/addCustomer.vue
+++ src/views/customer/customerInfo/addCustomer.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="customerInfo">
-    新建客户啊啊啊
-    <el-button type="primary">主要按钮</el-button>
+  <div class="addCustomer">
+    <div class="content">
+      <span class="lt">客户信息</span>
+      <span class="fr"><span class="red">* </span>为必填项</span>
+    </div>
   </div>
 </template>
 
@@ -16,4 +18,26 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .addCustomer {
+    padding: 20px;
+    .content {
+      height: 52px;
+      line-height: 52px;
+      background: #f3f8ff;
+      border: 1px solid #e6e6e6;
+      padding: 0 20px;
+      .lt {
+        color: #333;
+        font-size: 16px;
+      }
+      .fr {
+        float: right;
+        color: #666;
+        font-size: 16px;
+        .red {
+          color: #E02020;
+        }
+      }
+    }
+  }
 </style>
